231928d4924adf03aa1037d33abed236e1058de8
hoangdv198 <hoangdv198@gmail.com>
2025-01-02 09:49:14 +0700

    Initial Commit

---
+++ AppTestFeatureFacebook/AppTestFeatureFacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTestFeatureFacebookApp.swift
+//  AppTestFeatureFacebook
+//
+//  Created by Hoang Dang on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AppTestFeatureFacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
